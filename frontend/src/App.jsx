3748e9388a411df98b8cbea70f130fdf403834ce
mattSchaefer <matthew.schaefer12@gmail.com>
2025-05-30 22:53:17 -0400

    auth providers google, facebook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import P5Canvas from './components/P5Canvas.jsx'
+//import { firebaseConfig } from './config/firebase/firebase.js'
+import { signInWithGoogle, signInWithFacebook, signInWithTwitter } from './config/firebase/auth.js'
+import { initializeApp } from "firebase/app";
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
 function App() {
   const [count, setCount] = useState(0)
   const [message, setMessage] = useState("")
@@ -21,6 +33,17 @@ function App() {
           })
         }
     }
+    
+    const handleLogin = async (method) => {
+      try {
+        const result = await method();
+        const user = result.user;
+        console.log("Signed in user:", user);
+        user.getIdToken().then((token) => {console.log(token)})
+      } catch (error) {
+        console.error("OAuth Error:", error.message);
+      }
+  };
   useEffect(() => {
     fetch(rq.url, rq.options)
       .then(response => response.json())
@@ -41,12 +64,18 @@ function App() {
             <button className="header-btn">Learn More</button>
             <button id="get-started-btn" className="header-btn">Get Started</button>
           </span>
-          
+           <div className="p-4">
+            <button className="header-btn" onClick={() => handleLogin(signInWithGoogle)}>Sign in with Google</button>
+            <button className="header-btn" onClick={() => handleLogin(signInWithFacebook)}>Sign in with Facebook</button>
+            <button className="header-btn" onClick={() => handleLogin(signInWithTwitter)}>Sign in with X (Twitter)</button>
+          </div>
         </span>
+        
+       
+      
         <P5Canvas />
       </div>
-      <div className="section" id="section-2">
-      </div>
+      <div className="section" id="section-2"></div>
     </div>
   );
 }
